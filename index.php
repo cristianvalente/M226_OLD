d0d8d83e2447d44bd0f7adc36741105239590ccd
Cristian Valente <cristian.valente15@gmail.com>
2018-10-19 14:45:56 +0200

    lköjn

--- index.php
+++ index.php
@@ -12,14 +12,14 @@
     <div id="head">
         <h1>Jobcenter Inshallah</h1>
     </div>
-    nav>
-  <ul>
-    <li><a href="home.html">home</a></li>
-    <li><a href="angebot.html">angebot</a></li>
-    <li><a href="ueberuns.html">Ueber uns</a></li>
-    <li><a href="upload.html">upload</a></li>
-  </ul>
-</nav>
+        <nav>
+        <ul>
+        <li><a href="home.html">home</a></li>
+        <li><a href="angebot.html">angebot</a></li>
+        <li><a href="ueberuns.html">Ueber uns</a></li>
+        <li><a href="upload.html">upload</a></li>
+        </ul>
+        </nav>
     </div>
     <div id="inhalt">
         <p>Temporärjobs</p>   
