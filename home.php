470f2fb4f3501e04410ee8f8ac287bc37a5a6907
Cristian Valente <cristian.valente15@gmail.com>
2018-10-19 14:27:56 +0200

    new

---
+++ home.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>Lueg sin gsicht</title>
+        <link  rel="stylesheet" type="text/css" href="home.css"/>    
+    </head>
+
+    <body>
+        <div id="webseite">
+        <div id="header"></div>
+        <div id="head">
+            <h1>Jobcenter Inshallah</h1>
+        </div>
+        <div id="menue">
+            <?php
+            include 'menue.html';
+            ?>  
+        </div>
+        </div>
+        <div id="inhalt">
+            <p>Amplus</p>   
+            <p>Was geht</p>
+            <p>Das isch de Home Site</p>
+        </div>
+
+    </body>
+</html>
