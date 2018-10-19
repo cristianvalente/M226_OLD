ebcfc48dbd870b419c66e7286d5c1361d1dc3015
Cristian Valente <cristian.valente15@gmail.com>
2018-10-19 14:09:29 +0200

    newwwww

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <body>
     <div id="webseite">
     <div id="header"></div>
+    <div id="head">
         <h1>Jobcenter Inshallah</h1>
     </div>
     <div id="menue">
@@ -17,11 +18,12 @@
         ?>    
     </div>
     </div>
-    
     <div id="inhalt">
         <p>Dies ist ein Text</p>   
-        <p>Hallooooooooo test</p>
+        <p>Valente</p>
+        <p>Boss</p>
     </div>
+
 </body>
 
 
